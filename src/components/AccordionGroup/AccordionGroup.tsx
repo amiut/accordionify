d193935aa7c020c9410047d868b3ac0fa2e8c9d6
Amin A. Rezapour <amin.pe94@gmail.com>
2021-12-24 17:31:57 +0330

    className support

--- src/components/AccordionGroup/AccordionGroup.tsx
+++ src/components/AccordionGroup/AccordionGroup.tsx
@@ -4,25 +4,27 @@ import { forwardRef } from 'react';
 
 import IAccordionGroup from './IAccordionGroup';
 
-const AccordionGroup = forwardRef<HTMLDivElement, IAccordionGroup>(({ children, atomic = false, ...props }, ref) => {
-  const [storedRef, setStoredRef] = useState<RefObject<HTMLDivElement> | undefined>();
-  const refi = useRef<HTMLDivElement>(null);
+const AccordionGroup = forwardRef<HTMLDivElement, IAccordionGroup>(
+  ({ children, className, atomic = false, ...props }, ref) => {
+    const [storedRef, setStoredRef] = useState<RefObject<HTMLDivElement> | undefined>();
+    const refi = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (refi) {
-      setStoredRef(refi);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [refi.current]);
+    useEffect(() => {
+      if (refi) {
+        setStoredRef(refi);
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [refi.current]);
 
-  return (
-    <div ref={refi} {...props}>
-      <AccordionGroupProvider wrapper={storedRef} atomic={atomic}>
-        <div ref={ref}>{children}</div>
-      </AccordionGroupProvider>
-    </div>
-  );
-});
+    return (
+      <div ref={refi} className={`accordions-group ${atomic ? 'is-atomic' : ''} ${className}`} {...props}>
+        <AccordionGroupProvider wrapper={storedRef} atomic={atomic}>
+          <div ref={ref}>{children}</div>
+        </AccordionGroupProvider>
+      </div>
+    );
+  },
+);
 
 AccordionGroup.displayName = 'AccordionGroup';
 
--- src/components/AccordionGroup/IAccordionGroup.d.ts
+++ src/components/AccordionGroup/IAccordionGroup.d.ts
@@ -3,5 +3,6 @@ import { ReactNode } from 'react';
 export default interface IAccordionGroup {
   children: ReactNode;
   atomic?: boolean;
+  className: string;
   [props]?: any;
 }
